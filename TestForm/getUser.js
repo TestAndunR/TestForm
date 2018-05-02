07ae89436a29c4abea86289c590242787f4951ef
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-02 15:02:35 +0530

    add a scan database

--- TestForm/getUser.js
+++ TestForm/getUser.js
@@ -1,6 +1,21 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	let email = event.email;
+
+	ddb.scan({
+		TableName: 'userDetails', ExpressionAttributeValues: { ':email': email }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+			console.log("data");
+		} else {
+			console.log("Success");
+			//your logic goes here
+		}
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
