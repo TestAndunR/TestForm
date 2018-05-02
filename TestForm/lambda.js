f23a41750c3d72a34cedb3a85dfe65ddf731e32e
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-02 11:53:24 +0530

    user details put

---
+++ TestForm/lambda.js
@@ -0,0 +1,24 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	email = event.email
+	username = event.name;
+	phone = event.phone;
+	address = event.address;
+	ddb.put({
+		TableName: 'userDetails',
+		Item: { 'username': username, 'email': email, 'address': address, 'phone': phone }
+	}, function (err, data) {
+		if (err) {
+			console.log("Details coould not be entered");
+		} else {
+			//your logic goes here
+			console.log(data)
+			console.log("Details entered succesfully");
+		}
+	});
+
+
+
+	callback(null, 'Successfully executed');
+}
