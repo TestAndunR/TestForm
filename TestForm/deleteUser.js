12fa4b964ec407e3dccfe2267f1c116aed1e72ad
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-03 14:19:23 +0530

    Add s3 with delete object

--- TestForm/deleteUser.js
+++ TestForm/deleteUser.js
@@ -1,6 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
+	let name = event.name;
 
+	console.log(name);
+	s3.deleteObject({
+		'Bucket': "userdetail.s3.bucket",
+		'Key': name
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			console.log("Successfully deleted");
+			/*
+				data = {}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
